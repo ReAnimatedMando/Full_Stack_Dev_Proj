fbbb9da777e8e96c93de75235221d99a8d80284f
Aaron <160440038+ReAnimatedMando@users.noreply.github.com>
2025-01-10 12:36:29 -0700

    js
    troubleshooting using instructors updated code

--- server.js
+++ server.js
@@ -1,5 +1,57 @@
-// Step 1. Define the web scraper
+// step 1 - define the web scraper
 
-// Step 2. Initialize server that serves up an html file that the user can play with
+const cheerio = require('cheerio')
 
-// Step 3. Define api endpoints to access stock data (and call webscraper)
+let stockTicker = 'pypl'
+let type = 'history'
+
+async function scrapeData(ticker) {
+    try {
+        // step a - fetch the page html
+        const url = `https://finance.yahoo.com/quote/${ticker}/${type}?p=${ticker}`
+        const res = await fetch(url)
+        const html = await res.text()
+
+        const $ = cheerio.load(html)
+        const price_history = getPrices($)
+        return price_history
+        console.log(price_history)
+    } catch (err) {
+        console.log(err.message)
+    }
+}
+
+async function fetchHTML(ticker, type) {
+    const url = `https://finance.yahoo.com/quote/${ticker}/${type}?p=${ticker}`
+    let headers = {
+        "Content-Type": "application/json",
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+        'Accept-Language': 'en-US,en;q=0.9',
+        'Referer': 'https://www.google.com/',
+        'Connection': 'keep-alive',
+    }
+    const res = await fetch(url, {
+        method: 'GET',
+        headers: headers
+    })
+    const html = await res.text()
+    return html
+    const $ = cheerio.load(html)
+}
+
+function getPrices(cher) {
+    const prices = cher('td:nth-child(6)').get().map((current_value) => {
+        return cher(current_value).text()
+    })
+    return prices
+}
+
+scrapeData()
+
+// step 2 - initialize server that serves up an html file that the user can play with
+
+
+
+// step 3 - define api endpoints to access stock data (and call webscraper)
+

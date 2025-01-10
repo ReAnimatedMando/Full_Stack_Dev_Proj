a07ee84f3415e346dbf9c9d8157cbb9572f54817
Aaron <160440038+ReAnimatedMando@users.noreply.github.com>
2025-01-10 11:31:39 -0700

    js
    defined steps needed to build webscraper

---
+++ server.js
@@ -0,0 +1,5 @@
+// Step 1. Define the web scraper
+
+// Step 2. Initialize server that serves up an html file that the user can play with
+
+// Step 3. Define api endpoints to access stock data (and call webscraper)
